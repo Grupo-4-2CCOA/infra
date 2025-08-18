66ba8c5188a478754f6e00cea55638e2966799d9
LucasNagasse <lucas.nagasse@sptech.school>
2025-08-17 23:07:23 -0300

    feat: adicionando instâncias

--- diagrama.py
+++ diagrama.py
@@ -1,5 +1,17 @@
 from diagrams import Cluster, Diagram, Edge
 from diagrams.onprem.database import Mysql
+from diagrams.generic.os import Windows
+from diagrams.aws.compute import EC2
+from diagrams.aws.storage import EBS
+from diagrams.aws.network import VPC
 
 with Diagram("Arquitetura de Rede", show=False):
+  ec2_az1a_front = EC2("ec2_az1a_front")
+  ebs_ec2_az1a_front = EBS("ebs_ec2_az1a_front")
+
   database = Mysql("Banco de dados")
+  client = Windows("Cliente")
+
+  ec2_az1a_front << Edge() << ebs_ec2_az1a_front
+
+  client >> database
